db26d365ed27e2752c4218119b615918ea5cc761
Saruhan <saruhan.oymaci@hotmail.com>
2021-08-18 08:18:29 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import Search from "./component/Search"
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import Donation from "./component/Donation";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="cust-contain">
-<Router>
-  <Switch>
-    <Route path="/"></Route>
-  </Switch>
-</Router>
+          <Router>
+            <Switch>
+             <Route path="/"> <Donation/> </Route>
+             <Route path="/search"> <Search /> </Route>
+            </Switch>
+          </Router>
         </div>
       </div>
      
